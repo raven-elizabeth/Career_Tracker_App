ed6593461149719cb95dd1ed2c09ba07b3825120
Raven <157051380+raven-elizabeth@users.noreply.github.com>
2026-02-24 21:33:21 +0000

    Start WxH should be user's screen size & minimums equal to small smartphone

--- gui/root.py
+++ gui/root.py
@@ -2,13 +2,13 @@ from tkinter import Tk
 
 
 class Root(Tk):
-    START_WIDTH = 1280
-    START_HEIGHT = 800
     MIN_WIDTH = 360
     MIN_HEIGHT = 640
 
     def __init__(self):
         super().__init__()
-        self.geometry(f"{self.START_WIDTH}x{self.START_HEIGHT}")
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        self.geometry(f"{screen_width}x{screen_height}")
         self.minsize(width=self.MIN_WIDTH, height=self.MIN_HEIGHT)
         self.title("Career Tracker App")
