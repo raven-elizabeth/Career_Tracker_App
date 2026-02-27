bbefb7e3792b375fed40aed310801f31c473e1af
Raven <157051380+raven-elizabeth@users.noreply.github.com>
2026-02-27 18:18:41 +0000

    Added comment to explain fields.py file

--- domain/fields.py
+++ domain/fields.py
@@ -1,3 +1,6 @@
+# Fields are stored in a separate file to avoid circular imports and to make it easier to manage them in one place.
+# This helps to encourage separation of concerns and open/closed principle.
+
 FIELDS = [
     "date",
     "work_contribution",
