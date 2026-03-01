8eed1066aaea7e5dbbafe4edd5add80ec9b6adff
Raven <157051380+raven-elizabeth@users.noreply.github.com>
2026-03-01 23:03:37 +0000

    Added docstring to Root class

--- gui/root.py
+++ gui/root.py
@@ -8,8 +8,15 @@ from tkinter import Tk
 
 
 class Root(Tk):
-    # Minimum sizes chosen to ensure all content fits and is not cut off.
-    # Would like to add responsive design in the future for smaller sizes, but constrained by time and scope of project.
+    """The Root class is the main window of the application.
+    It sets the window title, minimum size, and starts maximised
+    to provide a consistent user experience.
+    It is passed as the parent to all screens,
+    which are then packed and unpacked to navigate between them.
+    Minimum sizes are chosen to ensure all content fits and is not cut off.
+    Would like to add responsive design in the future for smaller sizes,
+    but constrained by time and scope of project.
+    """
     MIN_WIDTH = 1400
     MIN_HEIGHT = 800
     APP_NAME = "Career Tracker App"
